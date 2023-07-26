2f70a296e8bc8a9c163e3c94425fa0c5a8e5b81e
Aimé <aime.manao@gmail.com>
2023-07-26 15:05:09 +0300

    initialisation projet

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $("nav ul li a").click(function(e) {
+        e.preventDefault();
+        var targetSection = $(this).attr("href");
+        var offset = $(targetSection).offset().top;
+
+        $("html, body").animate({
+            scrollTop: offset
+        }, 1000);
+    });
+
+    // Retarde l'ajout de la classe pour démarrer l'animation
+    setTimeout(function() {
+        $(".move-left").addClass("move-left-animation");
+    }, 1000); // Délai en millisecondes
+
+      // Activer le carrousel Bootstrap
+      $("#myCarousel").carousel({
+        interval: 3000 // Temps en millisecondes entre chaque image (3 secondes dans cet exemple)
+    });
+});
